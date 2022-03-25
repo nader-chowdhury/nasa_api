251eb8ecd2e0d04aa495ca50b831cdf6f5ec1aa6
Nader Chowdhury <nader.528@gmail.com>
2022-03-24 22:52:23 -0400

    setup events router

--- server/server.js
+++ server/server.js
@@ -1,19 +1,18 @@
 import fetch from "node-fetch";
 import express from "express";
+
 const server = express();
 const PORT = 5000;
 const API_KEY = "qEyHcD0VLOdWyRqdYK8n95OeU9FnVQk9ZxBomape";
 
-server.get("/", (req, res) => {
-    (async () => {
-        try {
-            const response = await fetch("https://eonet.gsfc.nasa.gov/api/v3/events");
-            const data = await response.json();
-            res.send(data);
-        } catch (error) {
-            console.log(error);    
-        }
-    })();
+// ROUTER IMPORTS
+import eventsRouter from "./routes/events.js";
+
+// ROUTER HANDLERS
+server.use("/events", eventsRouter);
+
+server.get("/",(req, res) => {
+    res.send("hellot");
 });
 
 server.listen(PORT, () => {
