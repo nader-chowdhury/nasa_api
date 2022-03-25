6b86f9976bb0b9ac91a17feb1a51a06f0f86b0e0
Nader Chowdhury <nader.528@gmail.com>
2022-03-24 20:44:45 -0400

    fetch

--- server/server.js
+++ server/server.js
@@ -1,9 +1,19 @@
-const express = require("express");
+import fetch from "node-fetch";
+import express from "express";
 const server = express();
 const PORT = 5000;
+const API_KEY = "qEyHcD0VLOdWyRqdYK8n95OeU9FnVQk9ZxBomape";
 
-server.get("/", (request, response) => {
-    response.send("Hello World!");
+server.get("/", (req, res) => {
+    (async () => {
+        try {
+            const response = await fetch("https://eonet.gsfc.nasa.gov/api/v3/events");
+            const data = await response.json();
+            res.send(data);
+        } catch (error) {
+            console.log(error);    
+        }
+    })();
 });
 
 server.listen(PORT, () => {
