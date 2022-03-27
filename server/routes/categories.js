ced2692ff138a44b2be3a8592e299f186ba627d8
Nader Chowdhury <nader.528@gmail.com>
2022-03-26 20:56:41 -0400

    added more routes

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.route("/")
     .get((req, res) => {
         (async function () {
-            const response = await fetch("https://eonet.gsfc.nasa.gov/api/v3/events");
+            const response = await fetch("https://eonet.gsfc.nasa.gov/api/v3/categories");
             const data = await response.json();
             res.send(data);
         })();
--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-import fetch from "node-fetch";
 import express from "express";
 
 const server = express();
@@ -7,9 +6,13 @@ const API_KEY = "qEyHcD0VLOdWyRqdYK8n95OeU9FnVQk9ZxBomape";
 
 // ROUTER IMPORTS
 import eventsRouter from "./routes/events.js";
+import categoriesRouter from "./routes/categories.js";
+import layersRouter from "./routes/layers.js";
 
 // ROUTER HANDLERS
 server.use("/events", eventsRouter);
+server.use("/categories", categoriesRouter);
+server.use("/layers", layersRouter);
 
 server.get("/",(req, res) => {
     res.send("hellot");
